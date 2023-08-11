9135994aa8e16f97218b0527109f0244522d9967
janosp207@gmail.com <>
2023-08-11 19:47:52 +0200

    feat(blog-post): small adjustments to blog post detail page

--- src/components/blogPost/PostBody.tsx
+++ src/components/blogPost/PostBody.tsx
@@ -2,6 +2,7 @@
 
 import { Post } from "@/classes/Post";
 import { Box, Typography, styled } from "@mui/material";
+import BlogTag from "@/components/utils/BlogTag";
 
 const StyledBlogBody = styled(Box)({
   marginTop: '1rem',
@@ -16,13 +17,20 @@ const StyledBlogText = styled(Typography)({
 const PostBody = ({ post }: { post: Post }): JSX.Element => {
   return (
     <StyledBlogBody>
-      <Typography variant='h6' fontWeight='bold' >{post.date}</Typography>
+      <Box display='flex' flexDirection='row' flexWrap='wrap' gap={3}>
+        <Typography variant='h6' fontWeight='bold' >{post.date}</Typography>
+        {
+          post.tags.map((tag) => (
+            <BlogTag tag={tag} key={`${post.title}-${tag}`} />
+          ))
+        }
+      </Box>
       <Typography variant='h1' fontWeight='bold'>{post.title}</Typography>
       <Typography variant='h6' fontWeight='bold' >({post.shortText})</Typography>
 
       <Box mt={5}>
         {post.text.map((text, index) => (
-          <StyledBlogText key={index} paragraph>{text}</StyledBlogText>
+          <StyledBlogText key={`${index}-paragraph`} paragraph>{text}</StyledBlogText>
         ))}
       </Box>
     </StyledBlogBody>
